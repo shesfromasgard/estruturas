7dc92bac04cbbad55580d9df515fea41d84ea6b4
laura🍧 <72207838+shesfromasgard@users.noreply.github.com>
2024-10-24 08:52:54 -0300

    Update Ex4.java

--- Aula 1 - Recursividade/Ex4.java
+++ Aula 1 - Recursividade/Ex4.java
@@ -6,21 +6,24 @@ public class Ex4 {
 
         int n = 0;
         int n1 = 0;
+        
+        n = sc.nextInt();
+        n1 = sc.nextInt();
 
-        while(n < 0)
-            n = sc.nextInt();
-
-        while(n1 < 0)
-            n1 = sc.nextInt();
-
-        System.out.println(recur(n, n1));
+        try {
+            System.out.println(recur(n, n1));
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 
         sc.close();
     }
 
-    public static long recur(int n, int n1) {
+    public static long recur(int n, int n1) throws Exception {
         if (n == 0 || n1 == 0)
             return 0;
+        else if(n1 < 0 || n < 0)
+            throw new Exception("Digite um número positivo.");
         else
            return n1 + recur(n - 1, n1);
     }
