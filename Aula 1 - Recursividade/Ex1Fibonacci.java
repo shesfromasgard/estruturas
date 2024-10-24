18aeca1b436acca8cc32ddf6b76d00c24acf8939
laura🍧 <72207838+shesfromasgard@users.noreply.github.com>
2024-10-24 08:51:49 -0300

    Update Ex1Fibonacci.java

--- Aula 1 - Recursividade/Ex1Fibonacci.java
+++ Aula 1 - Recursividade/Ex1Fibonacci.java
@@ -4,26 +4,37 @@ public class Ex1Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int num = -1;
+        int cont = 0;
 
-        while(num < 0)
-            num = sc.nextInt();
+        cont = sc.nextInt();
 
-        if(num == 0 || num == 1) 
-            System.out.println(num);
-        else if(num == 2)
-            System.out.println(1);
-        else {
-            int vet[] = new int[num];
-            vet[0] = 1;
-            vet[1] = 1;
-
-            for(int i = 2; i < num; i++)
-                vet[i] = vet[i - 1] + vet[i - 2];
-
-            System.out.println(vet[num - 1]);
+        try {
+            System.out.println(fibonacci(cont));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
 
         sc.close();
+    }
+
+    public static int fibonacci(int cont) throws Exception {
+
+        if(cont < 0)
+            throw new Exception("Digite um número positivo.");
+
+        if(cont == 0)
+            return 0;
+        else if (cont == 1 || cont == 2)
+            return 1;
+        else {
+            int vet[] = new int [cont];
+            vet[0] = 1;
+            vet[1] = 1;
+
+            for(int i = 2; i < cont; i++) {
+                vet[i] = vet[i - 1] + vet[i - 2];
+            }
+            return vet[cont - 1];
+        }
     }
 }
