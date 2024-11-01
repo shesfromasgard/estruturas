1611907f8c3d5816b1fe5a7bcf5a8906e5b264cd
laura🍧 <72207838+shesfromasgard@users.noreply.github.com>
2024-11-01 11:11:24 -0300

    Update Ex1.java

--- Aula 3 - Operações com Vetores/Ex1.java
+++ Aula 3 - Operações com Vetores/Ex1.java
@@ -1,25 +1,50 @@
 public class Ex1 {
+    
+    static int n = 10;
+
+    static int vet[];
+
     public static void main(String[] args) {
         
+        vet = aleatorio();
+        
+        System.out.println("Vetor padrão:\n");
+        print(vet);
+
+        vet = remove(vet);
+
+        System.out.println("\nVetor sem números pares:\n");
+        print(vet);
+        
+    }
+    
+    public static int[] aleatorio() {
         int vet [] = new int[10];
-        int n = 0;
+        
+        for(int i = 0; i < vet.length; i++)
+        vet[i] = (int) (Math.random() * 100) + 0;
+        
+        return vet;
+        
+    }
+    
+    public static void print(int vet[]) {
+        for(int i = 0; i < n; i++)
+            System.out.println(vet[i]);
+    }
+    
+    public static int[] remove(int vet[]) {
+
+        n = 0;
 
         for(int i = 0; i < vet.length; i++) {
-            vet[i] = (int) (Math.random() * 100) + 0;
-            System.out.println(vet[i] + " ");
-        }
-
-        for(int i = 0; i < vet.length; i++) {
-            if(vet[i] % 2 == 0) {
+            if(vet[i] % 2 == 1) {
                 vet[n] = vet[i];
                 n++;
             }
         }
 
-        System.out.println("\n");
-
-        for(int i = 0; i < n; i++)
-            System.out.println(vet[i]);
-
+        return vet;
+        
     }
 }
