996455b5acf2ebdf61893c828fe05ecec59bf698
laura🍧 <72207838+shesfromasgard@users.noreply.github.com>
2024-11-12 21:04:13 -0300

    Add files via upload

---
+++ Aula 3 - Operações com Vetores/Ex5/Comparacao O(n²)/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void print(int vet[]) {
+        boolean check[] = new boolean[vet.length];
+        int value = -1;
+        
+        for(int i = 0; i < vet.length; ++i) {
+            int minimum = 2147483647;
+            for(int j = 0; j < vet.length; ++j) {
+                if((vet[j] < minimum) && !check[j]) {
+                    minimum = vet[j];
+                    value = j;
+                }
+            }
+            check[value] = true;
+            System.out.println(minimum);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] vet = {12, 1, 5, 8, 21, 3, 17, 14, 1, 20, 7, 19, 6, 30, 10, 25, 9, 18, 22, 13, 16, 4, 2, 11, 15, 29, 27, 28, 24, 26, 23};
+
+        print(vet);
+
+    }
+}
