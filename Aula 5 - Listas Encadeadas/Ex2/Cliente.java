cb5143a655f3d6cd0a2012697b9b5e34ed9fdedf
laura🍧 <72207838+shesfromasgard@users.noreply.github.com>
2024-11-28 11:22:31 -0300

    Add files via upload

---
+++ Aula 5 - Listas Encadeadas/Ex2/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+    private int codigo;
+    private String nome;
+    private String telefone;
+
+    public Cliente(int codigo, String nome, String telefone) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public Cliente() {
+
+    }
+
+    
+}
