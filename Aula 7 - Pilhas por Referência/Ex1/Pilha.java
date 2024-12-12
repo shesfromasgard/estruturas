51d6e815630603b9fe2923c27cddddeb8c91b5b8
laura🍧 <72207838+shesfromasgard@users.noreply.github.com>
2024-12-12 20:14:45 -0300

    Add files via upload

---
+++ Aula 7 - Pilhas por Referência/Ex1/Pilha.java
@@ -0,0 +1,12 @@
+public class Pilha {
+    int carta;
+    Pilha proximo;
+    public Pilha(int carta, Pilha proximo) {
+        this.carta = carta;
+        this.proximo = proximo;
+    }
+
+    public Pilha() {
+        
+    }
+}
