b4d48939e73c7f3d291c255cb4f8895984713035
laura🍧 <72207838+shesfromasgard@users.noreply.github.com>
2024-12-12 20:02:11 -0300

    Add files via upload

--- Aula 7 - Pilhas por Referência/Ex2/Pilha.java
+++ Aula 7 - Pilhas por Referência/Ex2/Pilha.java
@@ -3,37 +3,35 @@ public class Pilha {
     private No controle;
 
     public void doIt(int count) throws Exception {
-        if(count <= 0)
+        if (count <= 0)
             throw new Exception("Digite um número maior que 0.");
-        else {
 
-            for(int i = 1; i <= count; ++i) {
-                No elemento;
-                if(i == 1 || i == 2) {
-                    elemento = new No(null, 1);
-                } else {
-                    int newElement = controle.elemento + controle.proximo.elemento;
-                    elemento = new No(controle, newElement);
-                }
-                controle = elemento;
-
-                tamanho++;
+        for (int i = 1; i <= count; ++i) {
+            No elemento;
+            if (i == 1 || i == 2) {
+                elemento = new No(controle, 1);
+            } else {
+                int newElement = controle.elemento + controle.proximo.elemento;
+                elemento = new No(controle, newElement);
             }
+            controle = elemento;
+            tamanho++;
         }
     }
 
     public int[] print() throws Exception {
-        if(tamanho == 0)
+        if (tamanho == 0)
             throw new Exception("Pilha vazia.");
-        else {
-            int[] array = new int[tamanho];
-            No el = controle.proximo;
-    
-            for(int i = 0; i < tamanho; ++i) {
-                array[i] = el.elemento;
-                el = el.proximo;
-            }
-            return array;
+
+        int[] array = new int[tamanho];
+        No atual = controle;
+        int index = 0;
+
+        while (atual != null) {
+            array[index++] = atual.elemento;
+            atual = atual.proximo;
         }
+
+        return array;
     }
 }
