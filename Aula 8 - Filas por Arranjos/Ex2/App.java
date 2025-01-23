afc1cd03992866ba3a64d83d4aff035062819a32
laura🍧 <72207838+shesfromasgard@users.noreply.github.com>
2025-01-22 21:35:48 -0300

    Add files via upload

--- Aula 8 - Filas por Arranjos/Ex2/App.java
+++ Aula 8 - Filas por Arranjos/Ex2/App.java
@@ -4,9 +4,8 @@ public class App {
     public static void main(String[] args) {
         Fila queue = new Fila();
         Scanner sc = new Scanner(System.in);
-        int op = 0;
 
-        while(op == 0) {
+        while (true) {
             System.out.println("FILA DE PROCESSOS");
             System.out.println("1 - Incluir processo;");
             System.out.println("2 - Retirar processo com mais tempo de espera;");
@@ -15,90 +14,75 @@ public class App {
             System.out.println("5 - Limpar fila de processos;");
             System.out.println("6 - Sair.");
 
-            op = sc.nextInt();
-
+            int op = sc.nextInt();
             sc.nextLine();
 
-            switch(op) {
+            switch (op) {
                 case 1:
                     try {
                         System.out.println("Identificação: ");
                         int id = sc.nextInt();
-
                         sc.nextLine();
-
                         System.out.println("Nome: ");
                         String nome = sc.nextLine();
-
                         queue.addHandler(new Processo(id, nome));
-
                         System.out.println("Processo adicionado com sucesso!");
-                    } catch(Exception ex) {
+                    } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
-                    op = 0;
                     break;
 
                 case 2:
                     try {
                         Processo tmp = queue.remove();
-
-                        System.out.println(tmp.id);
-                        System.out.println(tmp.nome);
-                    } catch(Exception ex) {
+                        System.out.println("Removido:");
+                        System.out.println("ID: " + tmp.id + ", Nome: " + tmp.nome);
+                    } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
-                    op = 0;
                     break;
 
                 case 3:
                     try {
-                        Processo array[] = queue.print();
-                        for(int i = 0; i < array.length; ++i) {
-                            System.out.println(array[i].id);
-                            System.out.println(array[i].nome);
-                            System.out.println("\n");
+                        Processo[] array = queue.print();
+                        for (Processo p : array) {
+                            System.out.println("ID: " + p.id + ", Nome: " + p.nome);
+                            System.out.println("-----------");
                         }
-                    } catch(Exception ex) {
+                    } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
-                    op = 0;
                     break;
 
                 case 4:
                     try {
                         System.out.println("Id: ");
                         int id = sc.nextInt();
-
-                        sc.nextLine();
-
                         int resultado = queue.search(id);
-
-                        if (resultado == -1)
+                        if (resultado == -1) {
                             System.out.println("Processo não encontrado.");
-                        else 
+                        } else {
                             System.out.println("Processo na posição " + resultado);
-                    } catch(Exception ex) {
+                        }
+                    } catch (Exception ex) {
                         System.out.println(ex.getMessage());
                     }
-                    op = 0;
                     break;
+
                 case 5:
                     queue.clear();
-                    System.out.println("Limpo!");
-                    op = 0;
+                    System.out.println("Fila limpa!");
                     break;
+
                 case 6:
                     System.out.println("Saindo...");
+                    sc.close();
                     System.exit(0);
-                    break;
+
                 default:
                     System.out.println("Digite uma opção válida, por favor.");
-                    op = 0;
                     break;
             }
         }
-
-        sc.close();
     }
 }
--- Aula 8 - Filas por Arranjos/Ex2/Fila.java
+++ Aula 8 - Filas por Arranjos/Ex2/Fila.java
@@ -6,72 +6,61 @@ public class Fila {
     private Processo array[] = new Processo[MAX_SIZE];
 
     public void addHandler(Processo p) throws Exception {
-        if(isEmpty())
+        if (isEmpty()) {
             add(p);
-        else
-            if(search(p.id) == -1)
-                throw new Exception("Esse processo já existe.");
-            else
-                add(p);
-
+        } else if (search(p.id) != -1) {
+            throw new Exception("Esse processo já existe.");
+        } else {
+            add(p);
+        }
     }
 
     public void add(Processo p) throws Exception {
-        if(isFull())
-            throw new Exception("Fia vazia.");
-        else {
-            if(fim == MAX_SIZE)
-                fim = 0;
-
+        if (isFull()) {
+            throw new Exception("Fila cheia.");
+        } else {
             array[fim] = p;
-
+            fim = (fim + 1) % MAX_SIZE;
             tamanho++;
         }
     }
 
     public Processo remove() throws Exception {
-        if(isEmpty())
+        if (isEmpty()) {
             throw new Exception("Fila vazia.");
-        else {
+        } else {
             Processo tmp = array[inicio];
-            inicio ++;
-            if(inicio == MAX_SIZE)
-                inicio = 0;
-
+            inicio = (inicio + 1) % MAX_SIZE;
             tamanho--;
             return tmp;
         }
     }
 
     public Processo[] print() throws Exception {
-        if(isEmpty())
+        if (isEmpty()) {
             throw new Exception("Fila vazia.");
-        else {
+        } else {
             Processo tmp_array[] = new Processo[tamanho];
             int index = inicio;
-            for(int i = 0; i < tamanho; ++i) {
-                tmp_array[i] = array[index++];
-                if(index == MAX_SIZE)
-                    index = 0;
+            for (int i = 0; i < tamanho; ++i) {
+                tmp_array[i] = array[index];
+                index = (index + 1) % MAX_SIZE;
             }
-
             return tmp_array;
         }
     }
 
     public int search(int id) throws Exception {
-        if(isEmpty())
+        if (isEmpty()) {
             throw new Exception("Fila vazia.");
-        else {
+        } else {
             int index = inicio;
-            for(int i = 0; i < tamanho; ++i) {
-                if(array[index++].id == id)
+            for (int i = 0; i < tamanho; ++i) {
+                if (array[index].id == id) {
                     return i + 1;
-                
-                if(index == MAX_SIZE)
-                    index = 0;
+                }
+                index = (index + 1) % MAX_SIZE;
             }
-
             return -1;
         }
     }
