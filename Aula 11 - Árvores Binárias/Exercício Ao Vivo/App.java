e83c2701f65d77b26d5991a7c22de3be5a455a37
laura🍧 <72207838+shesfromasgard@users.noreply.github.com>
2025-03-06 08:33:13 -0300

    Update App.java

--- Aula 11 - Árvores Binárias/Exercício Ao Vivo/App.java
+++ Aula 11 - Árvores Binárias/Exercício Ao Vivo/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Back back = new Back();
 
